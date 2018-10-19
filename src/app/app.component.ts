0ba8b1379b6ba0e5ffb7f43835ec386fc63d9acb
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-10-19 07:12:49 +0300

    add quote.ts class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+quotes: string[];
+
+constructor(){
+
+  this.quotes = ["A stone is heavy, sandy is weight but a fools wrath is heavier than the two combined","The higher you go the cooler it becomes"];
+}
 }
