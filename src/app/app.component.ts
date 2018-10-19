1d761438712bc9d14ae5cd8c229b2092e2585542
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-10-19 11:31:43 +0300

    generated pipe date-count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,11 @@ import {Quote} from './quote'
 export class AppComponent {
 
   quotes = [
-    new Quote(1,'Those who dare to fail miserably can achieve greatly.','By John .F. Kennedy'),
-    new Quote(2,'Get busy living or get busy dying.','By Stephen King'),
-    new Quote(3,'You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.','By Dr.Suess'),
-    new Quote(4,'Life is what happens when you are busy making other plans','John Lennon'),
-    new Quote(5,'A stone is heavy, sandy is weight but a fools wrath is heavier than the two combined','By Boyd'),
+    new Quote(1,'Those who dare to fail miserably can achieve greatly.','By John .F. Kennedy'new Date(1989,2,18)),
+    new Quote(2,'Get busy living or get busy dying.','By Stephen King',new Date(1938,8,13)),
+    new Quote(3,'You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.','By Dr.Suess',new Date(1973,7,19)),
+    new Quote(4,'Life is what happens when you are busy making other plans','John Lennon',new Date(1996,5,17)),
+    new Quote(5,'A stone is heavy, sandy is weight but a fools wrath is heavier than the two combined','By Boyd',new Date(2018,9,18)),
   ]
 
 
