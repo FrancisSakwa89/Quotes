dff82b17195b3c2482a50fe44c2d628ee155361b
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-10-19 08:10:06 +0300

    modified app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,16 @@ import { Component } from '@angular/core';
   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-quotes: string[];
+quotes = [
+  new Quote(1,'Those who dare to fail miserably can achieve greatly.'),
+  new Quote(2,'Get busy living or get busy dying.'),
+  new Quote(3,'You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.'),
+  new Quote(4,'Life is what happens when you are busy making other plans'),
+  new Quote(5,'A stone is heavy, sandy is weight but a fools wrath is heavier than the two combined'),
+]
 
-constructor(){
-
-  this.quotes = ["A stone is heavy, sandy is weight but a fools wrath is heavier than the two combined","The higher you go the cooler it becomes"];
-}
+// constructor(){
+//
+//   this.quotes = ["A stone is heavy, sandy is weight but a fools wrath is heavier than the two combined","The higher you go the cooler it becomes"];
+// }
 }
